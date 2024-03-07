09cdd41de6f80521f800d142d22547012734c3b2
pcanabarro <pedroh.canabarro@gmail.com>
2024-03-07 18:55:53 -0300

    token contract example WIP

---
+++ TokenContract/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Token {
+  string public name;
+  string public symbol();
+
+  // function symbol() public view returns(string) {
+
+  // }
+
+  // function name() public view returns(string memory) {
+  //   return name;
+  // }
+
+  // function setName(string memory _name) public view returns(string) {
+  //   name = _name;
+  // }
+}
